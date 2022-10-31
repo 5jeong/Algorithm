65805fc8d24054eac9986d0e6fd5d6904512fdfd
5jeong <5jeong>
2022-11-01 02:25:49 +0900

    2022-11-01

---
+++ algorithmStudy/가장짧은문자거리.java
@@ -0,0 +1,28 @@
+package algorithmStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장짧은문자거리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        String t = st.nextToken();
+
+        for(int i=0;i<str.length();i++){
+            int ans=str.length();
+            for(int j=0;j<str.length();j++){
+                if (str.charAt(j) == t.charAt(0)) {
+                    ans = Math.min(ans,Math.abs((i-j)));
+                }
+            }
+            System.out.print(ans);
+            System.out.print(" ");
+        }
+    }
+}
+
+
---
+++ algorithmStudy/숫자만추출.java
@@ -0,0 +1,30 @@
+package algorithmStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 숫자만추출{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String temp="";
+        int ans=0;
+        int cnt=0;
+        for(char x : str.toCharArray()){
+            if(Character.isDigit(x)){
+                temp+=x;
+            }
+        }
+        ans = Integer.parseInt(temp); //문자열을 int형으로 변환
+//        for(int i=0;i<temp.length();i++){
+//            if(temp.charAt(i)!='0'){
+//                for(int j=i;j<temp.length();j++){
+//                    ans += temp.charAt(j);
+//                }
+//                break;
+//            }
+//        }
+        System.out.println(ans);
+    }
+}
