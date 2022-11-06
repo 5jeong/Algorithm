4b94a10373b2458489c4869f6e18c3c95de93e89
5jeong <wjdgns4385@naver.com>
2022-11-06 23:43:09 +0900

    2022-11-06 study

--- algorithmStudy/배열/등수구하기.java
+++ algorithmStudy/배열/등수구하기.java
@@ -1,7 +1,22 @@
 package algorithmStudy.배열;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 등수구하기 {
-    public static void main(){
+    public static void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] ans = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
 
     }
 }
