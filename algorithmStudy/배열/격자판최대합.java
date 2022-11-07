b319504dd373c90db34d70783312f74be9ee1849
5jeong <wjdgns4385@naver.com>
2022-11-08 00:02:01 +0900

    2022-11-08

--- algorithmStudy/배열/격자판최대합.java
+++ algorithmStudy/배열/격자판최대합.java
@@ -1,7 +1,12 @@
 package algorithmStudy.배열;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 격자판최대합 {
     public static void main(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
     }
 }
