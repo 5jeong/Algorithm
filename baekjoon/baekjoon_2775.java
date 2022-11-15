03a1c26d0e620c182e7a18f0f0ba9d2c085f9ffd
5jeong <wjdgns4385@naver.com>
2022-11-16 02:50:08 +0900

    2022-11-06 study

---
+++ baekjoon/baekjoon_2775.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
