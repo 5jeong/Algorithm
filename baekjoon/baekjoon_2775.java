733c0a0d2680b128b2ccc39abd6b1e68e22b83d7
5jeong <wjdgns4385@naver.com>
2022-11-18 01:25:15 +0900

    2022-11-18 study

--- baekjoon/baekjoon_2775.java
+++ baekjoon/baekjoon_2775.java
@@ -7,8 +7,23 @@ import java.io.InputStreamReader;
 public class baekjoon_2775 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-
+        int t = Integer.parseInt(br.readLine());
+        while(t>0){
+            t--;
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr =new int[15][15];
+            for(int i=0;i<15;i++){
+                arr[0][i]=i;
+                arr[i][1] = 1;
+            }
+            for(int i = 1 ;i <=k;i++ ){
+                for(int j=2;j<n+1;j++){
+                    arr[i][j] = arr[i-1][j] + arr[i][j-1];
+                }
+            }
+            System.out.println(arr[k][n]);
+            arr = new int[15][15];
+        }
     }
 }
