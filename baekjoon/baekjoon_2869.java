2772de35068de69ffb4c41a32bf3d816398ed3a1
5jeong <wjdgns4385@naver.com>
2022-11-24 15:26:07 +0900

    2022-11-24 study

---
+++ baekjoon/baekjoon_2869.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        int ans=0,idx=0;
+        ans = (v-b) / (a-b);
+        if((v-b) % (a-b) != 0 ){
+            ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
