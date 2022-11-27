3579764d25fa2a6c4caa42e3e2661898f5aa190c
5jeong <wjdgns4385@naver.com>
2022-11-28 02:45:37 +0900

    2022-11-28 study

---
+++ algorithmStudy/투포인터and슬라이딩윈도우/연속된자연수합.java
@@ -0,0 +1,30 @@
+package algorithmStudy.투포인터and슬라이딩윈도우;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 연속된자연수합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr= new int[n/2+1];
+        int ans=0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = i+1;
+        }
+        int lp=0;
+        for(int rp=0;rp<arr.length;rp++){
+            sum+=arr[rp];
+            while (sum > n) {
+                sum -= arr[lp];
+                lp++;
+            }
+            if(sum==n){
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
