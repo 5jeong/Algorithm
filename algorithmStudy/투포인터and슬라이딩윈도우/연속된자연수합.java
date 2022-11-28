94de89d8ef64e50153b8492e074f97f8218a393a
5jeong <wjdgns4385@naver.com>
2022-11-28 17:18:11 +0900

    2022-11-28

--- algorithmStudy/투포인터and슬라이딩윈도우/연속된자연수합.java
+++ algorithmStudy/투포인터and슬라이딩윈도우/연속된자연수합.java
@@ -11,17 +11,27 @@ public class 연속된자연수합 {
         int[] arr= new int[n/2+1];
         int ans=0;
         int sum=0;
-        for(int i=0;i<arr.length;i++){
-            arr[i] = i+1;
-        }
-        int lp=0;
-        for(int rp=0;rp<arr.length;rp++){
-            sum+=arr[rp];
-            while (sum > n) {
-                sum -= arr[lp];
-                lp++;
-            }
-            if(sum==n){
+//        for(int i=0;i<arr.length;i++){
+//            arr[i] = i+1;
+//        }
+//        int lp=0;
+//        for(int rp=0;rp<arr.length;rp++){
+//            sum+=arr[rp];
+//            while (sum > n) {
+//                sum -= arr[lp];
+//                lp++;
+//            }
+//            if(sum==n){
+//                ans++;
+//            }
+//        }
+        //수학적 풀이
+        int cnt=1;
+        n--;
+        while(n>0){
+            cnt++;
+            n-=cnt;
+            if(n%cnt==0){
                 ans++;
             }
         }
---
+++ algorithmStudy/투포인터and슬라이딩윈도우/최대길이연속부분수열.java
@@ -0,0 +1,38 @@
+package algorithmStudy.투포인터and슬라이딩윈도우;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대길이연속부분수열 {
+    public static void main(String[] main) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int ans=Integer.MIN_VALUE;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int cnt=0,len=0;
+        int lt=0;
+        for(int rt=0;rt<n;rt++){
+            if(arr[rt]==0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
+                lt++;
+            }
+            len = rt-lt+1;
+            ans = Math.max(ans,len);
+        }
+        System.out.println(ans);
+    }
+}
+
