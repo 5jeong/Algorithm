2e863683c6af3aa8fe38927ef76bcb03919ce44c
5jeong <wjdgns4385@naver.com>
2022-12-01 18:36:33 +0900

    2022-12-01 study

---
+++ algorithmStudy/해쉬and정렬지원set/매출액의종류.java
@@ -0,0 +1,30 @@
+package algorithmStudy.해쉬and정렬지원set;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+//슬라이딩 위도우, 투포인터, 해쉬
+public class 매출액의종류 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr=new int[n];
+        int[] ans = new int[n-k+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int lt=0;
+        for(int rp=0;rp<n;rp++){
+            HashMap<Integer,Integer> map = new HashMap<>();
+
+        }
+        for(int x : ans){
+            System.out.print(x+" ");
+        }
+    }
+}
---
+++ out/production/baekjoon_JAVA/algorithmStudy/해쉬and정렬지원set/README.md
@@ -0,0 +1,26 @@
+# Hash 알고리즘
+
+### 해시 선언  
+HashMap<Character,Integer> map = new HashMap<>(); 
+### 해시 Method   
+* 해시에 값넣기   
+→ map.put('a',3);     
+  
+* 해시의 key값   
+map.keySet();   
+  
+* a(key)값의 value값   
+map.get('a');   
+  
+* key의 value값의 default값을 0으로 설정  
+map.getOrDefault(x,0)  
+  
+* key값이 포함되어있는지 확인(True,False)   
+map.containsKey('a');   
+  
+* key의 개수   
+map.size();   
+  
+* key값 삭제   
+map.remove('a');  
+  
