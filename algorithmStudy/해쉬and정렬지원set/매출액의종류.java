df689564bf48a1339de6c85dead56297bdc3965d
5jeong <wjdgns4385@naver.com>
2022-12-04 01:56:21 +0900

    2022-12-04 study

--- algorithmStudy/해쉬and정렬지원set/매출액의종류.java
+++ algorithmStudy/해쉬and정렬지원set/매출액의종류.java
@@ -3,9 +3,9 @@ package algorithmStudy.해쉬and정렬지원set;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
-
 
 //슬라이딩 위도우, 투포인터, 해쉬 알고리즘
 public class 매출액의종류 {
@@ -15,16 +15,28 @@ public class 매출액의종류 {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int[] arr=new int[n];
-        int[] ans = new int[n-k+1];
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int lt=0;
-        for(int rp=0;rp<n;rp++){
-            HashMap<Integer,Integer> map = new HashMap<>();
-
+        for(int i =0;i<k;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
+        ans.add(map.size());
+
+        int lt=0;
+        for(int rt=k;rt<n;rt++){
+            map.put(arr[rt],map.getOrDefault(arr[rt],0)+1);
+            map.put(arr[lt],map.get(arr[lt])-1);
+            if(map.get(arr[lt])==0){
+                map.remove(arr[lt]);
+            }
+            lt++;
+            ans.add(map.size());
+        }
+
         for(int x : ans){
             System.out.print(x+" ");
         }
