f5a7adaa01ce3d274ef2055ffbf46744f7177bb7
5jeong <104816594+5jeong@users.noreply.github.com>
2022-12-09 02:15:27 +0900

    Update baekjoon_1085.java

--- baekjoon/baekjoon_1085.java
+++ baekjoon/baekjoon_1085.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class baekjoon_1085 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
