47bf2a7118021f64e1cbda24fbfec522d64307a4
5jeong <wjdgns4385@naver.com>
2023-01-27 20:49:38 +0900

    2023-01-27 study

---
+++ algorithmStudy/Recursive_Tree_DFS_BFE기초/부분집합구하기_DFS.java
@@ -0,0 +1,36 @@
+package algorithmStudy.Recursive_Tree_DFS_BFE기초;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부분집합구하기_DFS {
+    static int[] check;
+    static int n;
+    public static void DFS(int L){
+        if(L>n){
+            String temp ="";
+            for(int i=1;i<=n;i++){
+                if(check[i] == 1){
+                    temp += (i +" ");
+                }
+            }
+            if(temp.length()>0){
+                System.out.println(temp);
+            }
+        }
+        else{
+            check[L] = 1;
+            DFS(L+1);
+            check[L] = 0;
+            DFS(L+1);
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        check = new int[n+1];
+        DFS(1);
+    }
+}
---
+++ algorithmStudy/Recursive_Tree_DFS_BFE기초/이진트리순회_DFS.java
@@ -0,0 +1,34 @@
+package algorithmStudy.Recursive_Tree_DFS_BFE기초;
+//이진트리 순회(전위순회, 중위순회, 후위순회)
+class Node{
+    int data;
+    Node lt,rt;
+    public Node(int val){
+        data = val;
+        lt = rt =null;
+    }
+}
+public class 이진트리순회_DFS {
+    Node root;
+    public static void DFS(Node root){
+        if(root == null) return;
+        else{
+            // 전위 순회 System.out.print(root.data+" ");
+            DFS(root.lt);
+            // 중위 순회 System.out.print(root.data+" ");
+            DFS(root.rt);
+            // 후위 순회 System.out.print(root.data+" ");
+        }
+    }
+    public static void main(String[] args){
+        이진트리순회_DFS tree = new 이진트리순회_DFS();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt=new Node(4);
+        tree.root.lt.rt=new Node(5);
+        tree.root.rt.lt=new Node(6);
+        tree.root.rt.rt=new Node(7);
+        DFS(tree.root);
+    }
+}
