8ecf1b837e5805156c8975d30d953fec5b3faa9e
5jeong <wjdgns4385@naver.com>
2023-01-27 17:32:57 +0900

    2023-01-27 study

---
+++ algorithmStudy/Recursive_Tree_DFS_BFE기초/재귀함수.java
@@ -0,0 +1,22 @@
+package algorithmStudy.Recursive_Tree_DFS_BFE기초;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 재귀함수 {
+    public static void DFS(int n){
+        if(n == 0){
+            return;
+        }
+        else{
+            DFS(n-1);
+            System.out.print(n+" ");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        DFS(n);
+    }
+}
