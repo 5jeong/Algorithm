ef3af48eb3f3263eff06fcc1f296e19faea887c2
5jeong <wjdgns4385@naver.com>
2023-01-29 19:25:18 +0900

    2023-01-29 study

--- algorithmStudy/Recursive_Tree_DFS_BFS기초/그래프최단거리_BFS.java
+++ algorithmStudy/Recursive_Tree_DFS_BFS기초/그래프최단거리_BFS.java
@@ -4,35 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class 경로탐색_인접리스트 {
-    static int[] check;
-    static int n,m,ans=0;
+public class 그래프최단거리_BFS {
+    static int n;
+    static int m;
     static ArrayList<ArrayList<Integer>> graph;
-    public static int DFS(int v){
-        if(v==n){
-            ans++;
-        }
-        else{
-            for(int nx : graph.get(v)){
-                if(check[nx]==0){
-                    check[nx] = 1;
-                    DFS(nx);
-                    check[nx] = 0;
+    static int[] dis;
+    static int[] check;
+    public static void BFS(int v){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(v);
+        while(!q.isEmpty()){
+            int cv = q.poll();
+            for(int nv : graph.get(cv)){
+                if(check[nv]==0){
+                    q.offer(nv);
+                    dis[nv] = dis[cv] + 1;
+                    check[nv] = 1;
                 }
             }
         }
-        return ans;
+
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        dis = new int[n+1];
         check = new int[n+1];
         graph = new ArrayList<ArrayList<Integer>>();
-
         for(int i=0;i<=n;i++){
             graph.add(new ArrayList<Integer>());
         }
@@ -42,7 +47,10 @@ public class 경로탐색_인접리스트 {
             int b = Integer.parseInt(st.nextToken());
             graph.get(a).add(b);
         }
-        check[1]=1;
-        System.out.println(DFS(1));
+        check[1] = 1;
+        BFS(1);
+        for(int i=2;i<=n;i++){
+            System.out.println(i+" : "+dis[i]);
+        }
     }
 }
