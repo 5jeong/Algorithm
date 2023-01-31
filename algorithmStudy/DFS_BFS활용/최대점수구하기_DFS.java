a2ceb6380f19fc7ec1a66699475567ac9c02b710
5jeong <wjdgns4385@naver.com>
2023-01-31 22:44:52 +0900

    2023-01-31 study

---
+++ algorithmStudy/DFS_BFS활용/최대점수구하기_DFS.java
@@ -0,0 +1,42 @@
+package algorithmStudy.DFS_BFS활용;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대점수구하기_DFS {
+    static int n,m, ans=Integer.MIN_VALUE;
+    static int[] score;
+    static int[] time;
+    public static void DFS(int L, int time_sum,int score_sum){
+        if(time_sum > m){
+            return;
+        }
+        if(L == n){
+            if(score_sum > ans){
+                ans = score_sum;
+            }
+        }
+        else{
+            DFS(L+1,time_sum + time[L],score_sum + score[L]);
+            DFS(L+1,time_sum,score_sum);
+
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        score = new int[n];
+        time = new int[n];
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            score[i] = Integer.parseInt(st.nextToken());
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+        DFS(0,0,0);
+        System.out.println(ans);
+    }
+}
