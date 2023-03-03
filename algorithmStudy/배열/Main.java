469a38a3bf4f3027c5be8b543ab8adf6a6624f6d
5jeong <wjdgns4385@naver.com>
2023-03-04 01:00:01 +0900

    2023-03-04 review

--- algorithmStudy/배열/Main.java
+++ algorithmStudy/배열/Main.java
@@ -8,38 +8,27 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean isPirme(int n){
-        for(int i =2 ; i<n;i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        ArrayList<Integer> ans = new ArrayList<>();
+        int[] arr = new int[n+1];
+        int ans = 0;
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n; i++) {
-            int temp = arr[i];
-            int res = 0 ;
-            while(temp>0){
-                int t = temp % 10;
-                res = res *10 +t;
-                temp = temp/10;
+        int cnt=0;
+        for(int i =0;i<n;i++){
+            if( arr[i] == 1){
+                cnt++;
+                ans+=cnt;
             }
-            if(isPirme(res)==true && res !=1){
-                ans.add(res);
+            else{
+                cnt=0;
             }
         }
-        for(int x : ans){
-            System.out.print(x+" ");
-        }
+
+        System.out.println(ans);
     }
 }
--- baekjoon/baekjoon_2506.java
+++ baekjoon/baekjoon_2506.java
@@ -1,2 +1,31 @@
-package baekjoon;public class baekjoon_2506 {
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2506 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+        int ans = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int cnt=0;
+        for(int i =0;i<n;i++){
+            if( arr[i] == 1){
+                cnt++;
+                ans+=cnt;
+            }
+            else{
+                cnt=0;
+            }
+        }
+        System.out.println(ans);
+    }
 }
