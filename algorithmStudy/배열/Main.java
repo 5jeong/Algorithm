c9004e9dc55510e88715eaee65fb5bb9c1835625
5jeong <wjdgns4385@naver.com>
2023-03-16 22:18:19 +0900

    2023-03-16 review

--- algorithmStudy/배열/Main.java
+++ algorithmStudy/배열/Main.java
@@ -14,37 +14,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[m][n];
-        int ans=0;
-        for(int i=0;i<m;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for(int i=1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int cnt = 0;
-                for (int s = 0; s < m; s++) {
-                    int temp = 0, temp2 = 0;
-                    for (int k = 0; k < n; k++) {
-                        if (arr[s][k] == i) {
-                            temp = k;
-                        }
-                        if (arr[s][k] == j) {
-                            temp2 = k;
-                        }
-                    }
-                    if (temp < temp2) {
-                        cnt++;
-                    }
-                }
-                if (cnt == m) {
-                    ans++;
-                }
-            }
-        }
-        System.out.println(ans);
+        int temp = -n;
+        System.out.println(temp);
+        int k = 4-temp;
+        System.out.println(k);
     }
 }
--- algorithmStudy/정렬and이분탐색and결정알고리즘/Main.java
+++ algorithmStudy/정렬and이분탐색and결정알고리즘/Main.java
@@ -5,43 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int[] cache = new int[s];
         int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        st=new StringTokenizer(br.readLine());
+        for(int i =0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int cnt=0,temp=0;
-        for(int x : arr){
-            int idx =-1;
-            for(int i=0;i<s;i++){
-                if(cache[i]==x){
-                    idx = i;
-                }
-            }
-            if(idx==-1){
-                for(int j=s-1;j>0;j--){
-                    cache[j] = cache[j-1];
-                }
-                cache[0] = x;
-            }
-            else{
-                for(int j=idx;j>0;j--){
-                    cache[j] = cache[j-1];
-                }
-                cache[0] = x;
+        Arrays.sort(arr);
+        String ans = "U";
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==arr[i+1]){
+                ans="D";
+                break;
             }
         }
-        for(int x : cache){
-            System.out.print(x+" ");
-        }
+        System.out.println(ans);
     }
 }
