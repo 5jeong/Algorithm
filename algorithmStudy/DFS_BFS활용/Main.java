abb0a83391ef6186c3a27a2e83a6899746480454
5jeong <wjdgns4385@naver.com>
2023-03-29 20:24:04 +0900

    2023-03-29 review

--- algorithmStudy/DFS_BFS활용/Main.java
+++ algorithmStudy/DFS_BFS활용/Main.java
@@ -8,27 +8,16 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,r;
-    static int[][] arr = new int[34][34];
-    static int DFS(int n,int r) {
-        if(arr[n][r] !=0){
-            return arr[n][r];
-        }
-        if(r==0 || n==r){
-            return 1;
-        }
-        else{
-            arr[n][r] = DFS(n-1,r-1)+DFS(n-1,r);
-            return arr[n][r];
-        }
+    static int n,f;
+    static void DFS(int L) {
+
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        int ans = DFS(n,r);
-        System.out.println(ans);
+        f = Integer.parseInt(st.nextToken());
+
     }
 }
