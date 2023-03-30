fdab7560856c22742c428813834e6fc2f0817d50
5jeong <wjdgns4385@naver.com>
2023-03-31 00:22:26 +0900

    2023-03-31 review

--- algorithmStudy/DFS_BFS활용/Main.java
+++ algorithmStudy/DFS_BFS활용/Main.java
@@ -9,8 +9,45 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n,f;
-    static void DFS(int L) {
 
+    static int[] arr,pm,check;
+    static int[][] dy = new int[35][35];
+    static boolean flag = false;
+
+    static int combi(int n,int r){
+        if(dy[n][r] != 0){
+            return dy[n][r];
+        }
+        if(n == r || r==0){
+            return 1;
+        }
+        else{
+            return dy[n][r] = combi(n-1,r-1)+combi(n-1,r);
+        }
+    }
+
+    static void DFS(int L,int sum) {
+        if(flag==true){
+            return;
+        }
+        if(L==n){
+            if(sum == f){
+                flag=true;
+                for(int x : pm){
+                    System.out.print(x+" ");
+                }
+            }
+        }
+        else{
+            for(int i=1;i<=n;i++){
+                if(check[i] == 0){
+                    check[i] = 1;
+                    pm[L] = i;
+                    DFS(L + 1, sum + (arr[L] * pm[L]));
+                    check[i]=0;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -18,6 +55,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         f = Integer.parseInt(st.nextToken());
-
+        arr = new int[n];
+        pm = new int[n];
+        check = new int[n+1];
+        for(int i=0;i<n;i++){
+            arr[i] = combi(n-1,i);
+        }
+        DFS(0,0);
     }
 }
--- algorithmStudy/DFS_BFS활용/수열추측하기_DFS.java
+++ algorithmStudy/DFS_BFS활용/수열추측하기_DFS.java
@@ -8,54 +8,55 @@ import java.util.StringTokenizer;
 
 public class 수열추측하기_DFS {
     static int n,f;
-    static int[] check, arr, p;
-    static boolean flag = false;
-    static int[][] dy = new int[35][35];
-    static int combi(int n, int r){
 
-        if(dy[n][r] > 0){
+    static int[] arr,pm,check;
+    static int[][] dy = new int[35][35];
+    static boolean flag = false;
+
+    static int combi(int n,int r){
+        if(dy[n][r] != 0){
             return dy[n][r];
         }
-        if(n==r || r==0){
+        if(n == r || r==0){
             return 1;
         }
         else{
-            return dy[n][r]=combi(n-1,r-1)+combi(n-1,r);
+            return dy[n][r] = combi(n-1,r-1)+combi(n-1,r);
         }
     }
-    static void DFS(int L, int sum){
 
-        if(flag){
+    static void DFS(int L,int sum) {
+        if(flag==true){
             return;
         }
-        if(L == n){
-            if(sum==f){
+        if(L==n){
+            if(sum == f){
                 flag=true;
-                for(int x : p) System.out.print(x+" ");
+                for(int x : pm){
+                    System.out.print(x+" ");
+                }
             }
         }
         else{
-            for(int i=1; i<=n; i++){
-                if(check[i]==0){
-                    check[i]=1;
-                    p[L]=i;
-                    DFS(L+1, sum+(p[L]*arr[L]));
+            for(int i=1;i<=n;i++){
+                if(check[i] == 0){
+                    check[i] = 1;
+                    pm[L] = i;
+                    DFS(L + 1, sum + (arr[L] * pm[L]));
                     check[i]=0;
                 }
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n= Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         f = Integer.parseInt(st.nextToken());
-        check = new int[n+1];
         arr = new int[n];
-        p=new int[n];
+        pm = new int[n];
+        check = new int[n+1];
         for(int i=0;i<n;i++){
             arr[i] = combi(n-1,i);
         }
