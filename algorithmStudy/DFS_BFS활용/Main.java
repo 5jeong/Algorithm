25dd87419ce918aa8936052771dfa56b9f7fe191
5jeong <wjdgns4385@naver.com>
2023-04-02 20:33:18 +0900

    2023-04-02 review

--- algorithmStudy/DFS_BFS활용/Main.java
+++ algorithmStudy/DFS_BFS활용/Main.java
@@ -6,39 +6,49 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
+    static class Point {
+        int x, y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static int n;
     static int[][] board = new int[21][21];
-    static int[] dx = {1,0,-1,0,1,1,-1,-1};
-    static int[] dy = {0,1,0,-1,1,-1,1,-1};
+    static int[] dx = {1, 0, -1, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+    static Queue<Point> queue = new LinkedList<>();
 
-    static void DFS(int x,int y) {
-        board[x][y] = 0;
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
-                board[nx][ny] = 0;
-                DFS(nx, ny);
+    static void BFS() {
+        while (!queue.isEmpty()) {
+            Point temp = queue.poll();
+            for (int i = 0; i < 8; i++) {
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                    board[nx][ny] = 0;
+                    queue.offer(new Point(nx, ny));
+                }
             }
         }
-
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int ans=0;
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
-                    DFS(i, j);
+                    board[i][j] = 0;
+                    queue.offer(new Point(i, j));
+                    BFS();
                     ans++;
                 }
             }
--- algorithmStudy/DFS_BFS활용/섬나라아일랜드_BFS.java
+++ algorithmStudy/DFS_BFS활용/섬나라아일랜드_BFS.java
@@ -8,31 +8,28 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 섬나라아일랜드_BFS {
-    static class Point{
-        int x;
-        int y;
-        Point(int x,int y){
+    static class Point {
+        int x, y;
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
     static int n;
-    static int[][] board ;
-    static int[] dx = {1,0,-1,0,1,1,-1,-1};
-    static int[] dy = {0,1,0,-1,1,-1,1,-1};
-    static int ans =0;
+    static int[][] board = new int[21][21];
+    static int[] dx = {1, 0, -1, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+    static Queue<Main.Point> queue = new LinkedList<>();
 
-    static void BFS(int x,int y){
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(x,y));
-        while(!q.isEmpty()){
-            Point p = q.poll();
-            for(int i = 0; i < 8 ; i++){
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if(nx >=0 && nx < n && ny >=0 && ny < n && board[nx][ny] == 1){
+    static void BFS() {
+        while (!queue.isEmpty()) {
+            Main.Point temp = queue.poll();
+            for (int i = 0; i < 8; i++) {
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
                     board[nx][ny] = 0;
-                    q.offer(new Point(nx,ny));
+                    queue.offer(new Main.Point(nx, ny));
                 }
             }
         }
@@ -41,25 +38,24 @@ public class 섬나라아일랜드_BFS {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        board = new int[n][n];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j] == 1){
-                    ans++;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
                     board[i][j] = 0;
-                    BFS(i,j);
+                    queue.offer(new Main.Point(i, j));
+                    BFS();
+                    ans++;
                 }
             }
         }
-
         System.out.println(ans);
     }
-
 }
 
