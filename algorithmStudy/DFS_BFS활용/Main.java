17f11033fee89392afaf09e72b2b079bc1127db7
5jeong <wjdgns4385@naver.com>
2023-04-04 23:35:00 +0900

    2023-04-04 review

--- algorithmStudy/DFS_BFS활용/Main.java
+++ algorithmStudy/DFS_BFS활용/Main.java
@@ -6,29 +6,34 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+    static class Point{
+        int x,y;
+        Point(int x,int y){
+            this.x=x;
+            this.y=y;
         }
     }
-    static int n;
-    static int[][] board = new int[21][21];
-    static int[] dx = {1, 0, -1, 0, 1, 1, -1, -1};
-    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
-    static Queue<Point> queue = new LinkedList<>();
-
-    static void BFS() {
-        while (!queue.isEmpty()) {
-            Point temp = queue.poll();
-            for (int i = 0; i < 8; i++) {
-                int nx = temp.x + dx[i];
-                int ny = temp.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
-                    board[nx][ny] = 0;
-                    queue.offer(new Point(nx, ny));
+    static int n,m;
+    static ArrayList<Point> house = new ArrayList<>();
+    static ArrayList<Point> pizza = new ArrayList<>();
+    static int[][] city;
+    static int[] combi;
+    static int len,ans=0;
+    static void DFS(int L,int s) {
+        if (L == m) {
+            int dis = Integer.MAX_VALUE;
+            for (Point house : house) {
+                for (int x : combi) {
+                    dis = Math.min(dis, Math.abs(house.x - pizza.get(x).x) + Math.abs(house.y - pizza.get(x).y));
+                    System.out.print(dis + " ");
                 }
+            }
+            System.out.println();
+        }
+        else{
+            for(int i=s;i<len;i++){
+                combi[L] = i;
+                DFS(L+1,i+1);
             }
         }
     }
@@ -36,23 +41,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
+        m = Integer.parseInt(st.nextToken());
+        combi = new int[m];
+        city = new int[n][n];
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
-                    board[i][j] = 0;
-                    queue.offer(new Point(i, j));
-                    BFS();
-                    ans++;
+            for(int j=0;j<n;j++){
+                city[i][j] = Integer.parseInt(st.nextToken());
+                if(city[i][j]==1){
+                    house.add(new Point(i+1,j+1));
+                }
+                else if (city[i][j]==2){
+                    len++;
+                    pizza.add(new Point(i+1,j+1));
                 }
             }
         }
-        System.out.println(ans);
+        DFS(0,0);
     }
 }
