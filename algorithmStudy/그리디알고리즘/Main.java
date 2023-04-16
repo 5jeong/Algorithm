6d055de647dd7a4a838dffe618f4a084427e6575
5jeong <wjdgns4385@naver.com>
2023-04-16 19:22:56 +0900

    2023-04-16 review

--- algorithmStudy/그리디알고리즘/Main.java
+++ algorithmStudy/그리디알고리즘/Main.java
@@ -6,14 +6,5 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[] unf;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        unf = new int[n+1];
-
-    }
 }
--- algorithmStudy/그리디알고리즘/원더랜드_크루스칼.java
+++ algorithmStudy/그리디알고리즘/원더랜드_크루스칼.java
@@ -9,62 +9,64 @@ import java.util.StringTokenizer;
 
 public class 원더랜드_크루스칼 {
     static int[] unf;
-    static class Edge implements Comparable<Edge>{
-        int v1,v2,cost;
-        Edge(int v1,int v2, int cost) {
+
+    static class Edge implements Comparable<Edge> {
+        int v1, v2, cost;
+
+        Edge(int v1, int v2, int cost) {
             this.v1 = v1;
             this.v2 = v2;
             this.cost = cost;
         }
+
         @Override
         public int compareTo(Edge o) {
-            return this.cost- o.cost;
+            return this.cost - o.cost;
         }
     }
-    static int Find(int f){
-        if(unf[f] == f){
-            return f;
-        }
-        else{
-            return unf[f] = Find(unf[f]);
+
+    static int Find(int v) {
+        if (v == unf[v]) {
+            return v;
+        } else {
+            return unf[v] = Find(unf[v]);
         }
     }
-    static void Union(int a,int b){
+
+    static void Union(int a, int b) {
         int fa = Find(a);
         int fb = Find(b);
-        if(fa!=fb){
-            unf[fa] = unf[fb];
+        if (fa != fb) {
+            unf[fa] = fb;
         }
-
     }
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
-        ArrayList<Edge> arr = new ArrayList<>();
-        unf = new int[v+1];
-        for(int i=1;i<=v;i++){
+        unf = new int[v + 1];
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 1; i <= v; i++) {
             unf[i] = i;
         }
-        for(int i=0;i<e;i++){
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            arr.add(new Edge(a,b,c));
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(v1, v2, cost));
         }
-        Collections.sort(arr);
         int ans = 0;
-        for(Edge edge : arr){
-            int v1 = Find(edge.v1);
-            int v2 = Find(edge.v2);
-            if(v1 != v2){
-                ans+= edge.cost;
-                Union(edge.v1,edge.v2);
+        Collections.sort(edges);
+        for (Edge x : edges) {
+            if (Find(x.v1) != Find(x.v2)) {
+                ans += x.cost;
+                Union(x.v1, x.v2);
             }
         }
         System.out.println(ans);
-
     }
 }
