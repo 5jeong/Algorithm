6126b2195cdf070835a32d75def09f1c83a04650
5jeong <wjdgns4385@naver.com>
2023-04-17 01:36:23 +0900

    2023-04-17 review

--- algorithmStudy/다이나믹프로그래밍/Main.java
+++ algorithmStudy/다이나믹프로그래밍/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 계단오르기 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
