f429285f6fc40be544c55b3c17d5011e32d59493
5jeong <wjdgns4385@naver.com>
2023-04-18 19:08:44 +0900

    2023-04-18 review

--- algorithmStudy/다이나믹프로그래밍/Main.java
+++ algorithmStudy/다이나믹프로그래밍/Main.java
@@ -3,17 +3,16 @@ package algorithmStudy.다이나믹프로그래밍;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
+        int[] dp = new int[n+2];
         dp[1] = 1;
         dp[2] = 2;
-        for(int i=3;i<=n;i++){
+        for(int i=3;i<=n+1;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-        System.out.println(dp[n]);
+        System.out.println(dp[n+1]);
     }
 }
--- algorithmStudy/다이나믹프로그래밍/돌다리건너기.java
+++ algorithmStudy/다이나믹프로그래밍/돌다리건너기.java
@@ -7,14 +7,13 @@ import java.io.InputStreamReader;
 public class 돌다리건너기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n =  Integer.parseInt(br.readLine());
-        int ans = 0;
-        int[] arr = new int[n+2];
-        arr[1] = 1;
-        arr[2] = 2;
-        for(int i=3;i<n+2;i++){
-            arr[i] = arr[i-2] + arr[i-1];
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 2];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        System.out.println(arr[n+1]);
+        System.out.println(dp[n + 1]);
     }
 }
