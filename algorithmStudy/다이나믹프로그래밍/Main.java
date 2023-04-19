538ebec0b7dd2cd5272ed9d61bc2c747eec87442
5jeong <wjdgns4385@naver.com>
2023-04-19 23:30:31 +0900

    2023-04-19 review

--- algorithmStudy/다이나믹프로그래밍/Main.java
+++ algorithmStudy/다이나믹프로그래밍/Main.java
@@ -3,35 +3,29 @@ package algorithmStudy.다이나믹프로그래밍;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine() );
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int[] arr =  new int[n];
-        int[] dp = new int[n];
-        st=new StringTokenizer(br.readLine());
+        int[] coin = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int[] dp = new int[m+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] =0;
         for(int i=0;i<n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
-        }
-        dp[0]=1;
-        int ans=Integer.MIN_VALUE;
-
-        for(int i=1;i<n;i++){
-            int temp=1;
-            for(int j=0;j<i;j++){
-                if(arr[i] > arr[j]){
-                    temp = Math.max(temp,dp[j]+1);
-                }
+            for(int j=coin[i];j<=m;j++){
+                dp[j] = Math.min(dp[j],dp[j-coin[i]]+1);
             }
-            dp[i] = temp;
-
         }
-        for(int x : dp){
-            ans = Math.max(ans,x);
-        }
-        System.out.println(ans);
+        System.out.println(dp[m]);
     }
 }
--- algorithmStudy/다이나믹프로그래밍/동전교환_냅색알고리즘.java
+++ algorithmStudy/다이나믹프로그래밍/동전교환_냅색알고리즘.java
@@ -12,24 +12,21 @@ public class 동전교환_냅색알고리즘 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        Integer[] arr = new Integer[n];
+        int[] coin = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i =0;i<n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int[] dp = new int[m+1];
-        int ans=0;
-//        Arrays.sort(arr,Collections.reverseOrder());
-        Arrays.fill(dp,9999999);
-        dp[0]= 0;
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] =0;
         for(int i=0;i<n;i++){
-            for(int j = arr[i];j<=m;j++){
-                dp[j]=Math.min(dp[j],dp[j-arr[i]]+1);
+            for(int j=coin[i];j<=m;j++){
+                dp[j] = Math.min(dp[j],dp[j-coin[i]]+1);
             }
         }
-
         System.out.println(dp[m]);
     }
 }
