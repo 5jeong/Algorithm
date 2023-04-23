b70fc544e28d4e1c04e1a8c9f6e0fb7fdfb795cd
5jeong <wjdgns4385@naver.com>
2023-04-23 17:39:05 +0900

    2023-04-23 review

--- algorithmStudy/다이나믹프로그래밍/Main.java
+++ algorithmStudy/다이나믹프로그래밍/Main.java
@@ -3,28 +3,53 @@ package algorithmStudy.다이나믹프로그래밍;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 public class Main {
+    static class brick implements Comparable<brick>{
+        int s,h,w;
+        brick(int s,int h,int w){
+            this.s=s;
+            this.h=h;
+            this.w=w;
+        }
+
+        @Override
+        public int compareTo(brick o) {
+            return o.s-this.s;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int[] coin = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            coin[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int[] dp = new int[m+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] =0;
+        ArrayList<brick> bricks = new ArrayList<>();
         for(int i=0;i<n;i++){
-            for(int j=coin[i];j<=m;j++){
-                dp[j] = Math.min(dp[j],dp[j-coin[i]]+1);
-            }
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            bricks.add(new brick(s,h,w));
         }
-        System.out.println(dp[m]);
+        Collections.sort(bricks);
+        int[] dp=  new int[n];
+        dp[0] = bricks.get(0).h;
+        for(int i=1;i<n;i++){
+            int temp=0;
+            for(int j=0;j<i;j++){
+                if(bricks.get(i).w < bricks.get(i-1).w){
+                    temp=Math.max(temp,dp[j]);
+                }
+
+            }
+            dp[i] = temp + bricks.get(i).h;
+        }
+        int ans=0;
+        for(int x : dp){
+            ans = Math.max(ans,x);
+        }
+        System.out.println(ans);
     }
 }
