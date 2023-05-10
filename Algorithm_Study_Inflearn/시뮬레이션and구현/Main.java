cb1468330a9284145b5d9ba59d62d60061b4bd7a
5jeong <wjdgns4385@naver.com>
2023-05-10 16:44:56 +0900

    2023-05-10 review

--- Algorithm_Study_Inflearn/시뮬레이션and구현/Main.java
+++ Algorithm_Study_Inflearn/시뮬레이션and구현/Main.java
@@ -9,22 +9,26 @@ class Solution {
         for(int i=0;i<n;i++){
             answer[i] = (char)(i+65);
         }
-        for(int s=1;s<=5;s++){
 
-        }
+
         for(int i=0;i<ladder.length;i++){
-            for(int j =0;j<ladder[0].length;j++){
-                System.out.print(ladder[i][j]+" ");
+            for(int j =0;j<ladder[i].length;j++){
+                if(ladder[i][j]==0){
+                    continue;
+                }
+                char temp = ' ';
+                temp = answer[ladder[i][j]];
+                answer[ladder[i][j]] = answer[ladder[i][j]-1];
+                answer[ladder[i][j]-1] = temp;
             }
-            System.out.println();
         }
         return answer;
     }
     public static void main(String[] args){
         Solution T = new Solution();
         System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
-//        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
-//        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
-//        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
     }
 }
