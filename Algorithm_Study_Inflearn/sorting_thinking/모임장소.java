a435426f39f7aef6874c98f2c8df889b79ffebb2
5jeong <wjdgns4385@naver.com>
2023-07-22 21:24:00 +0900

    2023-07-22 study

---
+++ Algorithm_Study_Inflearn/sorting_thinking/모임장소.java
@@ -0,0 +1,19 @@
+package Algorithm_Study_Inflearn.sorting_thinking;
+
+import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
+
+public class 모임장소 {
+    public static int solution(int[][] board){
+        int answer=0;
+        int n = board[0].length;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        System.out.println(solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
+        System.out.println(solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
+    }
+}
