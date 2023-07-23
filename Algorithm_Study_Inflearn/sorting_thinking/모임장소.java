1049e5b6379ea8cea9a6c31b8b3e50181876871c
5jeong <wjdgns4385@naver.com>
2023-07-23 23:28:35 +0900

    2023-07-23 study

--- Algorithm_Study_Inflearn/sorting_thinking/모임장소.java
+++ Algorithm_Study_Inflearn/sorting_thinking/모임장소.java
@@ -2,11 +2,34 @@ package Algorithm_Study_Inflearn.sorting_thinking;
 
 import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class 모임장소 {
     public static int solution(int[][] board){
         int answer=0;
         int n = board[0].length;
-
+        ArrayList<Integer> row = new ArrayList<>();
+        ArrayList<Integer> col = new ArrayList<>();
+        int idx=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1){
+                   row.add(i);
+                   col.add(j);
+                }
+            }
+        }
+        col.sort((a,b)-> a-b);
+        int k = row.size();
+        int temp = k/2;
+        int x = row.get(temp);
+        int y = col.get(temp);
+        for(int i=0;i<k;i++){
+            answer+=Math.abs(row.get(i) - x);
+            answer+=Math.abs(col.get(i) - y);
+        }
 
         return answer;
     }
