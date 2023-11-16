5833c929b294695dbbb70dc693a9b603574ad5ba
5jeong <wjdgns4385@naver.com>
2023-11-17 01:21:58 +0900

    2023-11-17 review

--- swea/d3/Solution.java
+++ swea/d3/Solution.java
@@ -1,14 +1,10 @@
 package swea.d3;
 
-import java.util.Scanner;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class View
-{
+public class Solution {
     public static void main(String args[]) throws Exception {
 //        System.setIn(new FileInputStream("C:\\Users\\82109\\Desktop\\정훈\\알고리즘 공부\\Algorithm_Java\\swea\\d3\\input.txt"));
         Scanner sc = new Scanner(System.in);
