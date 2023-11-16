a15c6a6a3b03fb7dc1843227122ef44b919935c5
5jeong <wjdgns4385@naver.com>
2023-11-17 03:12:22 +0900

    2023-11-17 review

--- swea/d3/Solution.java
+++ swea/d3/Solution.java
@@ -1,32 +1,55 @@
 package swea.d3;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
+    static int cnt, ans;
+    static int[] arr;
+
+    static void dfs(int start, int change) {
+        if (change == cnt) {
+            String temp = "";
+            for (int x : arr) {
+                temp += x;
+            }
+            ans = Math.max(ans, Integer.parseInt(temp));
+            return;
+        }
+        for (int i = start; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                swap(i, j);
+                dfs(i, change + 1);
+                swap(i, j);
+            }
+        }
+    }
+
+    static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String args[]) throws Exception {
 //        System.setIn(new FileInputStream("C:\\Users\\82109\\Desktop\\정훈\\알고리즘 공부\\Algorithm_Java\\swea\\d3\\input.txt"));
         Scanner sc = new Scanner(System.in);
-//        int T = sc.nextInt();
-        int T = 10;
+        int T = sc.nextInt();
+//        int T = 10;
         for (int test_case = 1; test_case <= T; test_case++) {
-            int n = sc.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
+            String str = sc.next();
+            cnt = sc.nextInt();
+            ans = Integer.MIN_VALUE;
+            arr = new int[str.length()];
+            for (int i = 0; i < str.length(); i++) {
+                arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
             }
-            int ans = 0;
-            for (int i = 2; i < n - 2; i++) {
-                int max = Integer.MIN_VALUE;
-                for (int j = i - 2; j <= i + 2; j++) {
-                    if (i == j) continue;
-                    max = Math.max(max, arr[j]);
-                }
-                if (arr[i] - max < 0) continue;
-                ans += arr[i] - max;
+            if (cnt > str.length()) {
+                cnt = str.length();
             }
+            dfs(0, 0);
             System.out.println("#" + test_case + " " + ans);
+
         }
     }
 }
--- swea/d3/최대상금.java
+++ swea/d3/최대상금.java
@@ -3,59 +3,53 @@ package swea.d3;
 import java.io.FileInputStream;
 import java.util.*;
 
-class 최대상금
-{
+class 최대상금 {
+    static int cnt, ans;
     static int[] arr;
-    static int ans,cnt;
-    static void dfs(int s,int change){
-        if(change == cnt){
-            String str = "";
-            for(int x : arr){
-                str+=x;
+
+    static void dfs(int start, int change) {
+        if (change == cnt) {
+            String temp = "";
+            for (int x : arr) {
+                temp += x;
             }
-            int num = Integer.parseInt(str);
-            ans = Math.max(ans,num);
+            ans = Math.max(ans, Integer.parseInt(temp));
             return;
-
         }
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                dfs(0,change+1);
-
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
+        for (int i = start; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                swap(i, j);
+                dfs(i, change + 1);
+                swap(i, j);
             }
         }
-
     }
-    public static void main(String args[]) throws Exception
-    {
-        System.setIn(new FileInputStream("C:\\Users\\82109\\Desktop\\정훈\\알고리즘 공부\\Algorithm_Java\\swea\\input.txt"));
 
+    static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String args[]) throws Exception {
+//        System.setIn(new FileInputStream("C:\\Users\\82109\\Desktop\\정훈\\알고리즘 공부\\Algorithm_Java\\swea\\d3\\input.txt"));
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+        int T = sc.nextInt();
+//        int T = 10;
+        for (int test_case = 1; test_case <= T; test_case++) {
             String str = sc.next();
             cnt = sc.nextInt();
-            ans=0;
-            arr =new int[str.length()];
-            int t=0;
-            for(int i=0;i<str.length();i++){
+            ans = Integer.MIN_VALUE;
+            arr = new int[str.length()];
+            for (int i = 0; i < str.length(); i++) {
                 arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
             }
-            if(cnt  > str.length()){
+            if (cnt > str.length()) {
                 cnt = str.length();
             }
-            dfs(0,0);
-            System.out.println("#"+test_case+" "+ans);
+            dfs(0, 0);
+            System.out.println("#" + test_case + " " + ans);
+
         }
     }
 }
