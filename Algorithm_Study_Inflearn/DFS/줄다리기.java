6e9218758c949eb60bc0acf5ddd21e319c6b86e1
5jeong <wjdgns4385@naver.com>
2024-02-01 00:02:42 +0900

    2024-02-01 review

--- Algorithm_Study_Inflearn/DFS/줄다리기.java
+++ Algorithm_Study_Inflearn/DFS/줄다리기.java
@@ -1,49 +1,64 @@
 package Algorithm_Study_Inflearn.DFS;
 
-import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
-
 import java.util.Stack;
 
 public class 줄다리기 {
-    static int answer,n;
-    static int[][] relation;
-    static Stack<Integer> stack ;
+    static int n = 7, answer;
+    static int[][] board;
     static int[] ch;
-    static void DFS(int L){
+    static Stack<Integer> stack;
+
+    //    static void dfs(int L, int a, int b) {
+//        if (board[a][b] == 1 ) {
+//            return;
+//        }
+//        if (L == n) {
+//            answer++;
+//        } else {
+//            for (int i = 1; i <= 7; i++) {
+//                if (ch[i] == 0) {
+//                    ch[i] = 1;
+//                    dfs(L + 1, b, i);
+//                    ch[i] = 0;
+//                }
+//            }
+//        }
+//
+//    }
+    static void dfs(int L){
         if(L==n){
             answer++;
         }
         else{
-            for(int i=1;i<=n;i++){
-                if(!stack.isEmpty() && relation[stack.peek()][i]==1){
-                    continue;
-                }
+            for(int i=1;i<=7;i++){
+                if(!stack.isEmpty() && board[stack.peek()][i]==1) continue;
                 if(ch[i]==0){
                     ch[i]=1;
                     stack.push(i);
-                    DFS(L+1);
+                    dfs(L+1);
                     ch[i]=0;
                     stack.pop();
                 }
             }
         }
-
     }
-    public static int solution(int[][] fight){
-        answer=0;
+
+    public static int solution(int[][] fight) {
+        answer = 0;
+        board = new int[8][8];
         ch = new int[8];
-        n=7;
-        stack=new Stack<>();
-        relation = new int[8][8];
-        for(int x[] : fight){
-            relation[x[0]][x[1]] = 1;
-            relation[x[1]][x[0]] = 1;
+        stack = new Stack<>();
+        for (int[] x : fight) {
+            board[x[0]][x[1]] = 1;
+            board[x[1]][x[0]] = 1;
         }
-        DFS(0);
+//        dfs(0, 0, 0);
+        dfs(0);
+
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(solution(new int[][]{{1, 3}, {5, 7}, {4, 2}}));
         System.out.println(solution(new int[][]{{3, 2}, {3, 5}, {5, 2}, {7, 3}}));
         System.out.println(solution(new int[][]{{1, 2}, {1, 5}, {1, 7}, {1, 3}}));
