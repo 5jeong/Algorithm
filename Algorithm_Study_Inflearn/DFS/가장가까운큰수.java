a6705960cccaac26287aa9feb63655dddc13debe
5jeong <wjdgns4385@naver.com>
2024-01-31 22:53:51 +0900

    2024-01-31 review

--- Algorithm_Study_Inflearn/DFS/가장가까운큰수.java
+++ Algorithm_Study_Inflearn/DFS/가장가까운큰수.java
@@ -1,58 +1,53 @@
 package Algorithm_Study_Inflearn.DFS;
 
-import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class 가장가까운큰수 {
-
     static ArrayList<Integer> nums;
-    static int[] check;
-    static int tanget,answer;
+    static int[] ch;
+    static int answer, target;
     static boolean flag;
-    static void DFS(int L,int n){
-        if(flag){
+
+    static void dfs(int L, int num) {
+        if (flag) {
             return;
         }
-        if(L== nums.size()){
-            if(tanget < n){
-                answer = n;
-                flag=true;
+        if (L == nums.size()) {
+            if (num > target) {
+                answer = num;
+                flag = true;
             }
-        }
-        else{
-            for(int i=0;i< nums.size();i++){
-                if(check[i]==0){
-                    check[i]=1;
-                    DFS(L+1,n*10+nums.get(i));
-                    check[i] =0;
+        } else {
+            for (int i = 0; i < nums.size(); i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    dfs(L + 1, num * 10 + nums.get(i));
+                    ch[i] = 0;
                 }
             }
+        }
 
-        }
     }
-    public static int solution(int n){
+
+    public static int solution(int n) {
+        target = n;
         answer = 0;
-        tanget = n;
-        flag = false;
         nums = new ArrayList<>();
-        int tmp= n;
-        while(tmp >0){
-            int t = tmp %10;
-            nums.add(t);
-            tmp /= 10;
+        flag = false;
+        while (n > 0) {
+            nums.add(n % 10);
+            n /= 10;
         }
-        check = new int[nums.size()];
-        nums.sort((a,b)-> a-b);
-        DFS(0,0);
-        if(flag ==false){
-            return -1;
+        ch = new int[nums.size()];
+        Collections.sort(nums);
+        dfs(0, 0);
+        if (!flag) {
+            answer = -1;
         }
         return answer;
     }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(solution(123));
         System.out.println(solution(321));
         System.out.println(solution(20573));
