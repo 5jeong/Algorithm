93418a5c0284d42b89f35be7d00efa683dcc9c01
5jeong <wjdgns4385@naver.com>
2024-02-04 19:04:54 +0900

    2024-02-03 review

--- Algorithm_Study_Inflearn/BFS/미로의최단거리경로.java
+++ Algorithm_Study_Inflearn/BFS/미로의최단거리경로.java
@@ -1,6 +1,5 @@
 package Algorithm_Study_Inflearn.BFS;
 
-import Algorithm_Study_Inflearn.BFS.Solution.Point;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -21,20 +20,20 @@ public class 미로의최단거리경로 {
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
         int[][] dist = new int[7][7];
-        Queue<Solution.Point> queue = new LinkedList<>();
-        queue.offer(new Solution.Point(0, 0));
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(0, 0));
         while (!queue.isEmpty()) {
             int len = queue.size();
             L++;
             for (int i = 0; i < len; i++) {
-                Solution.Point temp = queue.poll();
+                Point temp = queue.poll();
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = temp.x + dx[dir];
                     int ny = temp.y + dy[dir];
                     if (nx >= 0 && nx < 7 && ny >= 0 && ny < 7 && board[nx][ny] == 0) {
                         board[nx][ny] = 1;
                         dist[nx][ny] = L;
-                        queue.offer(new Solution.Point(nx, ny));
+                        queue.offer(new Point(nx, ny));
                     }
                 }
             }
