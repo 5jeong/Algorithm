f2537c8880800f145122466ad1d82047da1f068a
5jeong <wjdgns4385@naver.com>
2024-02-04 20:48:45 +0900

    2024-02-04 review

--- Algorithm_Study_Inflearn/BFS/숲속의기사.java
+++ Algorithm_Study_Inflearn/BFS/숲속의기사.java
@@ -1,76 +1,66 @@
 package Algorithm_Study_Inflearn.BFS;
 
-import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class 숲속의기사 {
-    static Queue<Point> queue;
-    static int answer,r,c;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] dis;
-
-    static int[][] ch;
-    static class Point{
+    static class Point {
         int x;
         int y;
-        Point(int x,int y){
-            this.x=x;
-            this.y=y;
-        }
-    }
-    static void BFS(int[][] board){
-        int L=0;
-        while(!queue.isEmpty()){
-            L++;
-            int len = queue.size();
-            for(int i=0;i<len;i++){
-                Point temp = queue.poll();
-                for(int j=0;j<4;j++){
-                    int nx= temp.x +dx[j];
-                    int ny = temp.y +dy[j];
-                    if (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != 1 && ch[nx][ny] == 0) {
-                        ch[nx][ny] = 1;
-                        queue.offer(new Point(nx, ny));
-                        dis[nx][ny] += L;
-                    }
-                }
-            }
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
     public static int solution(int[][] board) {
-        answer = Integer.MAX_VALUE;
-        queue = new LinkedList<>();
-        r = board.length;
-        c = board[0].length;
-        dis = new int[r][c];
-        ch = new int[r][c];
+        int answer = Integer.MAX_VALUE;
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int r = board.length;
+        int c = board[0].length;
+        int[][] dist = new int[r][c];
+        int[][] ch;
+        Queue<Point> queue = new LinkedList<>();
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 if (board[i][j] == 2 || board[i][j] == 3) {
+                    int L = 0;
+                    queue.offer(new Point(i, j));
                     ch = new int[r][c];
                     ch[i][j] = 1;
-                    queue.offer(new Point(i, j));
-                    BFS(board);
+                    while (!queue.isEmpty()) {
+                        L++;
+                        int len = queue.size();
+                        for (int s = 0; s < len; s++) {
+                            Point temp = queue.poll();
+                            for (int dir = 0; dir < 4; dir++) {
+                                int nx = temp.x + dx[dir];
+                                int ny = temp.y + dy[dir];
+                                if (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != 1 && ch[nx][ny] == 0) {
+                                    ch[nx][ny] = 1;
+                                    dist[nx][ny] += L;
+                                    queue.offer(new Point(nx, ny));
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        int res=0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if(board[i][j]==4){
-                  res = dis[i][j];
-                  answer = Math.min(answer,res);
+                if (board[i][j] == 4 && dist[i][j] > 0) {
+                    answer = Math.min(answer, dist[i][j]);
                 }
             }
         }
 
         return answer;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println(solution(new int[][]{{4, 1, 0, 0, 0, 0, 1, 0},
                 {0, 0, 0, 1, 0, 1, 0, 0},
                 {0, 2, 1, 1, 3, 0, 4, 0},
--- Algorithm_Study_Inflearn/BFS/집을짓자.java
+++ Algorithm_Study_Inflearn/BFS/집을짓자.java
@@ -6,88 +6,65 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class 집을짓자 {
-    static int answer,n;
-    static ArrayList<Integer> x = new ArrayList<>();
-    static ArrayList<Integer> y = new ArrayList<>();
-    static int[][]ch;
-    static int[] dx ={0,1,0,-1};
-    static int[] dy={1,0,-1,0};
-    static Queue<Point> queue;
-    static class Point{
+    private static class Point {
         int x;
         int y;
-        Point(int x,int y){
-            this.x=x;
-            this.y=y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
-    static int BFS(int[][] board){
-        int[][] dis=new int[n][n];
-        int L=0;
-        while(!queue.isEmpty()){
-            L++;
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                Point temp = queue.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = temp.x + dx[j];
-                    int ny = temp.y + dy[j];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && ch[nx][ny] == 0 && board[nx][ny]==1) {
-                        ch[nx][ny]=1;
-                        dis[nx][ny]=L;
-                    }
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && ch[nx][ny] == 0 && board[nx][ny] ==0 ) {
-                        dis[nx][ny] = L;
-                        ch[nx][ny] = 1;
-                        queue.offer(new Point(nx, ny));
-                    }
-                }
-            }
-        }
 
-        int res=0;
-        for(int i=0;i<x.size();i++){
-            int t = dis[x.get(i)][y.get(i)];
-            if(t==0){
-                return -1;
-            }
-            else{
-                res += t;
-            }
-        }
-
-        return res;
-    }
-    public static int solution(int[][] board){
-        n=board.length;
-        queue = new LinkedList<>();
-        answer = Integer.MAX_VALUE;
-        int res=0;
-        x = new ArrayList<>();
-        y = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++) {
+    public static int solution(int[][] board) {
+        int answer = Integer.MAX_VALUE;
+        int n = board.length;
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int[][] dist = new int[n][n];
+        int[][] ch;
+        Queue<Point> queue;
+        ArrayList<Point> buildings = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
-                    x.add(i);
-                    y.add(j);
+                    buildings.add(new Point(i, j));
                 }
             }
         }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ch=new int[n][n];
-                if(board[i][j]==0){
-                    ch[i][j]=1;
-                    queue.offer(new Point(i,j));
-                    res = BFS(board);
-                    if(res!=-1){
-                        answer = Math.min(answer,res);
+        int emptyLand = 0;
+        for (Point building : buildings) {
+            int L = 0;
+            ch = new int[n][n];
+            queue = new LinkedList<>();
+            queue.offer(building);
+            ch[building.x][building.y] = 1;
+            while (!queue.isEmpty()) {
+                L++;
+                int len = queue.size();
+                for (int i = 0; i < len; i++) {
+                    Point temp = queue.poll();
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = temp.x + dx[dir];
+                        int ny = temp.y + dy[dir];
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == emptyLand) {
+                            board[nx][ny]--;
+                            dist[nx][ny] += L;
+                            queue.offer(new Point(nx, ny));
+                        }
                     }
                 }
             }
+            emptyLand--;
         }
-        if(answer==Integer.MAX_VALUE){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == emptyLand && dist[i][j] > 0) {
+                    answer = Math.min(answer, dist[i][j]);
+                }
+            }
+        }
+        if (answer == Integer.MAX_VALUE) {
             return -1;
         }
         return answer;
