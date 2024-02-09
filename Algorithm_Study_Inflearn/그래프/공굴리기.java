c0d4639113a9939dacfa4dae53a9e25746830a09
5jeong <wjdgns4385@naver.com>
2024-02-09 19:25:07 +0900

    2024-02-09 review

--- Algorithm_Study_Inflearn/그래프/공굴리기.java
+++ Algorithm_Study_Inflearn/그래프/공굴리기.java
@@ -1,59 +1,68 @@
 package Algorithm_Study_Inflearn.그래프;
 
-import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
-
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class 공굴리기 {
-    public static int solution(int[][] board, int[] s, int[] e){
+    public static int solution(int[][] board, int[] s, int[] e) {
         int answer = 0;
-        int[] dx = {0,1,0,-1};
-        int[] dy = {1,0,-1,0};
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
         int n = board.length;
         int m = board[0].length;
         int[][] cost = new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(cost[i],Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[]{s[0], s[1], 0});
         cost[s[0]][s[1]] = 0;
-        pq.offer(new int[]{s[0],s[1],0});
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            if(cur[2] > cost[cur[0]][cur[1]]) continue;
-            for(int i=0;i<4;i++){
-                int nx = cur[0];
-                int ny = cur[1];
-                int len = cur[2];
+        while (!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            if (temp[2] > cost[temp[0]][temp[1]]) {
+                continue;
+            }
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = temp[0];
+                int ny = temp[1];
+                int len = temp[2];
                 while (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
-                    nx += dx[i];
-                    ny += dy[i];
                     len++;
+                    nx += dx[dir];
+                    ny += dy[dir];
                 }
-                nx-=dx[i];
-                ny-=dy[i];
+                nx -= dx[dir];
+                ny -= dy[dir];
                 len--;
-                if(cost[nx][ny] > len){
+                if (cost[nx][ny] > len) {
                     cost[nx][ny] = len;
-                    pq.offer(new int[]{nx,ny,cost[nx][ny]});
+                    pq.offer(new int[]{nx, ny, len});
                 }
-
             }
         }
-        if(cost[e[0]][e[1]]==Integer.MAX_VALUE){
-            answer = -1;
+        if (cost[e[0]][e[1]] == Integer.MAX_VALUE) {
+            return -1;
         }
-        else{
-            answer = cost[e[0]][e[1]];
-        }
+        answer = cost[e[0]][e[1]];
         return answer;
     }
-    public static void main(String[] args){
-        System.out.println(solution(new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0}, {1, 0, 0, 0, 0, 0}}, new int[]{1, 0}, new int[]{4, 5}));
-        System.out.println(solution(new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0}, {1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 2}));
-        System.out.println(solution(new int[][]{{1, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 0}}, new int[]{0, 3}, new int[]{4, 2}));
-        System.out.println(solution(new int[][]{{0, 1, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 1, 1, 0, 1, 1}, {0, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 5}));
-        System.out.println(solution(new int[][]{{0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 3}));
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(
+                new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0},
+                        {1, 0, 0, 0, 0, 0}}, new int[]{1, 0}, new int[]{4, 5}));
+        System.out.println(solution(
+                new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0},
+                        {1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 2}));
+        System.out.println(solution(
+                new int[][]{{1, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 0}},
+                new int[]{0, 3}, new int[]{4, 2}));
+        System.out.println(solution(
+                new int[][]{{0, 1, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 1, 1, 0, 1, 1},
+                        {0, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 5}));
+        System.out.println(solution(
+                new int[][]{{0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 3}));
     }
 }
--- Algorithm_Study_Inflearn/그래프/방향바꾸기.java
+++ Algorithm_Study_Inflearn/그래프/방향바꾸기.java
@@ -1,48 +1,58 @@
 package Algorithm_Study_Inflearn.그래프;
 
-import Algorithm_Study_Inflearn.해싱and시간파싱.Solution;
-
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class 방향바꾸기 {
     public static int solution(int[][] board) {
         int answer = 0;
-        int[] dx = {0,0,1,-1};
-        int[] dy = {1,-1,0,0};
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         int n = board.length;
         int m = board[0].length;
+        int[][] ch = new int[n][m];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[]{0, 0, 0});
         int[][] cost = new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(cost[i],Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[2]-b[2]);
-        pq.offer(new int[]{0,0,0});
-        cost[0][0]=0;
+        cost[0][0] = 0;
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            if(cur[2] > cost[cur[0]][cur[1]]) continue;
-            for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (board[cur[0]][cur[1]] - 1 == i && cost[nx][ny] > cur[2]) {
-                    pq.offer(new int[]{nx, ny, cur[2]});
-                    cost[nx][ny] = cur[2];
-                } else if (cost[nx][ny] > cur[2] + 1) {
-                    pq.offer(new int[]{nx, ny, cur[2] + 1});
-                    cost[nx][ny] = cur[2] + 1;
+            int[] temp = pq.poll();
+            if (cost[temp[0]][temp[1]] < temp[2]) {
+                continue;
+            }
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = temp[0] + dx[dir];
+                int ny = temp[1] + dy[dir];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+                if (board[temp[0]][temp[1]] - 1 == dir && cost[nx][ny] > temp[2]) {
+                    cost[nx][ny] = cost[temp[0]][temp[1]];
+                    pq.offer(new int[]{nx, ny, cost[nx][ny]});
+                } else if (cost[nx][ny] > temp[2] + 1) {
+                    cost[nx][ny] = temp[2] + 1;
+                    pq.offer(new int[]{nx, ny, cost[nx][ny]});
                 }
             }
         }
-        return cost[n-1][m-1];
+        answer = cost[n - 1][m - 1];
+        return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(solution(new int[][]{{3, 1, 3}, {1, 4, 2}, {4, 2, 3}}));
         System.out.println(solution(new int[][]{{3, 2, 1, 3}, {1, 1, 4, 2}, {3, 4, 2, 1}, {1, 2, 2, 4}}));
-        System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2}, {2, 1, 1, 1, 4, 2}, {2, 2, 2, 1, 2, 2}, {1, 3, 3, 4, 4, 4}, {1, 2, 2, 3, 3, 4}}));
-        System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2, 2, 2}, {2, 1, 1, 1, 4, 2, 1, 1}, {2, 2, 2, 1, 2, 2, 3, 4}, {1, 3, 3, 4, 4, 4, 3, 1}, {1, 2, 2, 3, 3, 4, 3, 4}, {1, 2, 2, 3, 3, 1, 1, 1}}));
-        System.out.println(solution(new int[][]{{1, 2, 3, 2, 1, 3, 1, 2, 2, 2}, {1, 2, 2, 1, 1, 1, 4, 2, 1, 1}, {3, 2, 2, 2, 2, 1, 2, 2, 3, 4}, {3, 3, 1, 3, 3, 4, 4, 4, 3, 1}, {1, 1, 1, 2, 2, 3, 3, 4, 3, 4}, {1, 1, 1, 2, 2, 3, 3, 1, 1, 1}}));
+        System.out.println(solution(
+                new int[][]{{3, 2, 1, 3, 1, 2}, {2, 1, 1, 1, 4, 2}, {2, 2, 2, 1, 2, 2}, {1, 3, 3, 4, 4, 4},
+                        {1, 2, 2, 3, 3, 4}}));
+        System.out.println(solution(
+                new int[][]{{3, 2, 1, 3, 1, 2, 2, 2}, {2, 1, 1, 1, 4, 2, 1, 1}, {2, 2, 2, 1, 2, 2, 3, 4},
+                        {1, 3, 3, 4, 4, 4, 3, 1}, {1, 2, 2, 3, 3, 4, 3, 4}, {1, 2, 2, 3, 3, 1, 1, 1}}));
+        System.out.println(solution(new int[][]{{1, 2, 3, 2, 1, 3, 1, 2, 2, 2}, {1, 2, 2, 1, 1, 1, 4, 2, 1, 1},
+                {3, 2, 2, 2, 2, 1, 2, 2, 3, 4}, {3, 3, 1, 3, 3, 4, 4, 4, 3, 1}, {1, 1, 1, 2, 2, 3, 3, 4, 3, 4},
+                {1, 1, 1, 2, 2, 3, 3, 1, 1, 1}}));
     }
 }
