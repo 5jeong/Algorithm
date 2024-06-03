66873b8142f323bcbeb3c3d274dee601fd43181a
5jeong <wjdgns4385@naver.com>
2024-06-03 23:17:02 +0900

    2024-06-03 study

---
+++ Promgrammers/level2/거리두기확인하기.java
@@ -0,0 +1,86 @@
+package Promgrammers.level2;
+
+import java.util.Arrays;
+
+/*
+문제 풀이 흐름
+1. 대기실의 모든 응시자(P)의 위치에 대해 반복
+    A. 상하좌우 중 빈 테이블(O)이 있는 방향에 대해 체크
+    B. 빈 테이블과 인접한 위치 중 응시자가 있다면 거리두기 x
+2. 거리 두기를 지키지 않은 경우를 발견하지 못하면 거리두기를 지킨것으로 (1) 출력 / 한명이라도 거리두기를 지키지 않으면(0) 출력
+ */
+public class 거리두기확인하기 {
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, -1, 1, 0};
+
+    public static void main(String[] args) {
+        String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {
+        "POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {
+        "OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+        int[] ans = solution(places);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        int idx = 0;
+        for (String[] place : places) {
+            char[][] room = new char[5][5];
+            for (int i = 0; i < 5; i++) {
+                room[i] = place[i].toCharArray();
+            }
+            if (isDistanced(room)) {
+                answer[idx] = 1;
+            } else {
+                answer[idx] = 0;
+            }
+            idx++;
+        }
+        return answer;
+    }
+
+    private static boolean isDistanced(char[][] room) {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                if (room[x][y] == 'P') {
+                    if (!isDistanced(room, x, y)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDistanced(char[][] room, int x, int y) {
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
+                if (room[nx][ny] == 'P') {
+                    return false;
+                }
+                if (room[nx][ny] == 'O') {
+                    if (!isNextDistanced(room, x, y, 3 - dir)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNextDistanced(char[][] room, int x, int y, int exclude) {
+        for (int dir = 0; dir < 4; dir++) {
+            if (dir == exclude) {
+                continue;
+            }
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && room[nx][ny] == 'P') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
