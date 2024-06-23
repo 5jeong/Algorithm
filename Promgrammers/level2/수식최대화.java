e8b9a42cd3fa2a3fa73d291c44f62bfdfd355a4c
5jeong <wjdgns4385@naver.com>
2024-06-23 21:23:18 +0900

    2024-06-23 study

--- Promgrammers/level2/수식최대화.java
+++ Promgrammers/level2/수식최대화.java
@@ -1,7 +1,6 @@
 package Promgrammers.level2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -14,7 +13,7 @@ public class 수식최대화 {
             "*-+".split(""),
             "*+-".split(""));
 
-    private long calculate(String op, long left, long right) {
+    private static long calculate(String op, long left, long right) {
         if (op.equals("+")) {
             return left + right;
         } else if (op.equals("-")) {
@@ -23,20 +22,26 @@ public class 수식최대화 {
         return left * right;
     }
 
-    private long calculate(List<String> tokens, String[] orderStep) {
+    private static Long calculate(List<String> tokens, String[] orderStep) {
         for (String op : orderStep) {
             for (int i = 0; i < tokens.size(); i++) {
-                if(tokens.get(i).equals(op)){
-                    long left = Long.parseLong(tokens.get(i-1));
-                    long right = Long.parseLong(tokens.get(i+1));
-                    long res = calculate(op,left,right);
+                if (tokens.get(i).equals(op)) {
+                    long left = Long.parseLong(tokens.get(i - 1));
+                    long right = Long.parseLong(tokens.get(i + 1));
+                    long res = calculate(op, left, right);
+                    tokens.remove(i + 1);
+                    tokens.remove(i);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(res));
+                    i -= 2;
                 }
             }
         }
+        return Long.parseLong(tokens.get(0));
 
     }
 
-    public long solution(String expression) {
+    public static long solution(String expression) {
         long answer = 0;
         StringTokenizer st = new StringTokenizer(expression, "+-*", true);
         List<String> tokens = new ArrayList<>();
@@ -44,18 +49,13 @@ public class 수식최대화 {
             tokens.add(st.nextToken());
         }
         for (String[] orderStep : orderSteps) {
-            calculate(tokens, orderStep);
+            answer = Math.max(answer, Math.abs(calculate(new ArrayList<>(tokens), orderStep)));
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        StringTokenizer st = new StringTokenizer("100-200*300-500+200", "+-*", true);
-        while (st.hasMoreTokens()) {
-            System.out.println(st.nextToken());
-        }
-        String s = "+-*";
-        String[] temp = s.split("");
-        System.out.println(Arrays.toString(temp));
+        System.out.println(solution("100-200*300-500+20"));
+        System.out.println(solution("50*6-3*2"));
     }
 }
