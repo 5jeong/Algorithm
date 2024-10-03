36429920823116917cdd235d78c36bf5046c68c9
5jeong <wjdgns4385@naver.com>
2024-10-03 18:06:40 +0900

    2024-10-04 study

--- Algorithm_Study_Inflearn/BFS/Solution.java
+++ Algorithm_Study_Inflearn/BFS/Solution.java
@@ -3,11 +3,11 @@ package Algorithm_Study_Inflearn.BFS;
 import java.util.*;
 
 class Solution {
-    static int[][] dis;
-    static int[][] ch;
-    static int n, emptyLand;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static int r, c;
+    static int[][] dis;
+    static int[][] cnt;
     static Queue<Point> queue;
 
     static class Point {
@@ -20,64 +20,87 @@ class Solution {
         }
     }
 
-
     static void bfs(int[][] board) {
         int L = 0;
+        int[][] ch = new int[r][c];
         while (!queue.isEmpty()) {
             int len = queue.size();
             L++;
             for (int i = 0; i < len; i++) {
                 Point temp = queue.poll();
+                ch[temp.x][temp.y] = 1;
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = temp.x + dx[dir];
                     int ny = temp.y + dy[dir];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == emptyLand) {
-                        board[nx][ny]--;
+
+                    if (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != 1 && board[nx][ny] != 3
+                            && ch[nx][ny] == 0) {
+                        ch[nx][ny] = 1;
+                        cnt[nx][ny]++;
                         queue.offer(new Point(nx, ny));
                         dis[nx][ny] += L;
                     }
                 }
             }
         }
-        emptyLand--;
     }
 
-    public int solution(int[][] board) {
+    public static int solution(int[][] board) {
         int answer = Integer.MAX_VALUE;
-        n = board.length;
-        emptyLand = 0;
-        dis = new int[n][n];
-        queue = new LinkedList<>();
-        ch = new int[n][n];
+        r = board.length;
+        c = board[0].length;
 
-        // 빌딩(1) 지점에서 bfs , 거리누적하기
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
+        // 영희와 산딸기 거리
+        dis = new int[r][c];
+        cnt = new int[r][c];
+        queue = new LinkedList<>();
+        List<Point> berries = new ArrayList<>();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                // 영희 위치
+                if (board[i][j] == 2) {
                     queue.offer(new Point(i, j));
                     bfs(board);
                 }
-            }
-        }
-
-        // 모두방문한곳에서 최소거리 구하기
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == emptyLand && dis[i][j] > 0) {
-                    answer = Math.min(answer, dis[i][j]);
+                // 기사 위치
+                if (board[i][j] == 3) {
+                    queue.offer(new Point(i, j));
+                    bfs(board);
                 }
+                if (board[i][j] == 4) {
+                    berries.add(new Point(i, j));
+                }
+
             }
         }
-
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        for (Point point : berries) {
+            int x = point.x;
+            int y = point.y;
+            if (dis[x][y] > 0 && cnt[x][y] == 2) {
+                answer = Math.min(answer, dis[x][y]);
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        System.out.println(T.solution(
-                new int[][]{{1, 0, 2, 0, 1}, {0, 0, 0, 0, 0}, {0, 2, 1, 0, 0}, {2, 0, 0, 2, 2}, {0, 0, 0, 0, 0}}));
-        System.out.println(T.solution(new int[][]{{1, 0, 0, 1}, {0, 0, 2, 0}, {0, 0, 1, 0}, {2, 2, 0, 0}}));
-        System.out.println(T.solution(new int[][]{{1, 2, 0, 0}, {0, 0, 1, 2}, {0, 2, 0, 0}, {0, 2, 1, 0}}));
-        System.out.println(T.solution(new int[][]{{1, 0, 0, 1}, {0, 0, 2, 0}, {0, 0, 1, 0}, {2, 2, 0, 1}}));
+        System.out.println(T.solution(new int[][]{{4, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 1, 0, 0},
+                {0, 2, 1, 1, 3, 0, 4, 0},
+                {0, 0, 0, 4, 1, 1, 1, 0}}));
+        System.out.println(T.solution(new int[][]{{3, 0, 0, 0, 1, 4, 4, 4},
+                {0, 1, 1, 0, 0, 0, 1, 0},
+                {0, 1, 4, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 1, 1, 0},
+                {4, 0, 0, 0, 1, 0, 0, 0},
+                {4, 1, 0, 0, 1, 0, 0, 0},
+                {4, 0, 0, 0, 0, 0, 1, 2}}));
+        System.out.println(T.solution(new int[][]{
+                {4, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0},
+                {0, 0, 2, 3, 4},
+                {0, 1, 0, 1, 0}}));
     }
 }
--- Algorithm_Study_Inflearn/BFS/숲속의기사.java
+++ Algorithm_Study_Inflearn/BFS/숲속의기사.java
@@ -1,9 +1,19 @@
 package Algorithm_Study_Inflearn.BFS;
 
+import Algorithm_Study_Inflearn.BFS.Solution.Point;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class 숲속의기사 {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int r, c;
+    static int[][] dis;
+    static int[][] cnt;
+    static Queue<Solution.Point> queue;
+
     static class Point {
         int x;
         int y;
@@ -14,58 +24,76 @@ public class 숲속의기사 {
         }
     }
 
-    public static int solution(int[][] board) {
-        int answer = Integer.MAX_VALUE;
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        int r = board.length;
-        int c = board[0].length;
-        int[][] dist = new int[r][c];
-        int[][] ch;
-        Queue<Point> queue = new LinkedList<>();
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (board[i][j] == 2 || board[i][j] == 3) {
-                    int L = 0;
-                    queue.offer(new Point(i, j));
-                    ch = new int[r][c];
-                    ch[i][j] = 1;
-                    while (!queue.isEmpty()) {
-                        L++;
-                        int len = queue.size();
-                        for (int s = 0; s < len; s++) {
-                            Point temp = queue.poll();
-                            for (int dir = 0; dir < 4; dir++) {
-                                int nx = temp.x + dx[dir];
-                                int ny = temp.y + dy[dir];
-                                if (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != 1 && ch[nx][ny] == 0) {
-                                    ch[nx][ny] = 1;
-                                    dist[nx][ny] += L;
-                                    queue.offer(new Point(nx, ny));
-                                }
-                            }
-                        }
+    static void bfs(int[][] board) {
+        int L = 0;
+        int[][] ch = new int[r][c];
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            L++;
+            for (int i = 0; i < len; i++) {
+                Solution.Point temp = queue.poll();
+                ch[temp.x][temp.y] = 1;
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = temp.x + dx[dir];
+                    int ny = temp.y + dy[dir];
+
+                    if (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != 1 && board[nx][ny] != 3
+                            && ch[nx][ny] == 0) {
+                        ch[nx][ny] = 1;
+                        cnt[nx][ny]++;
+                        queue.offer(new Solution.Point(nx, ny));
+                        dis[nx][ny] += L;
                     }
                 }
             }
         }
+    }
+
+    public static int solution(int[][] board) {
+        int answer = Integer.MAX_VALUE;
+        r = board.length;
+        c = board[0].length;
+
+        // 영희와 산딸기 거리
+        dis = new int[r][c];
+        cnt = new int[r][c];
+        queue = new LinkedList<>();
+        List<Solution.Point> berries = new ArrayList<>();
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (board[i][j] == 4 && dist[i][j] > 0) {
-                    answer = Math.min(answer, dist[i][j]);
+                // 영희 위치
+                if (board[i][j] == 2) {
+                    queue.offer(new Solution.Point(i, j));
+                    bfs(board);
                 }
+                // 기사 위치
+                if (board[i][j] == 3) {
+                    queue.offer(new Solution.Point(i, j));
+                    bfs(board);
+                }
+                if (board[i][j] == 4) {
+                    berries.add(new Solution.Point(i, j));
+                }
+
             }
         }
-
+        for (Solution.Point point : berries) {
+            int x = point.x;
+            int y = point.y;
+            if (dis[x][y] > 0 && cnt[x][y] == 2) {
+                answer = Math.min(answer, dis[x][y]);
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{4, 1, 0, 0, 0, 0, 1, 0},
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[][]{{4, 1, 0, 0, 0, 0, 1, 0},
                 {0, 0, 0, 1, 0, 1, 0, 0},
                 {0, 2, 1, 1, 3, 0, 4, 0},
                 {0, 0, 0, 4, 1, 1, 1, 0}}));
-        System.out.println(solution(new int[][]{{3, 0, 0, 0, 1, 4, 4, 4},
+        System.out.println(T.solution(new int[][]{{3, 0, 0, 0, 1, 4, 4, 4},
                 {0, 1, 1, 0, 0, 0, 1, 0},
                 {0, 1, 4, 0, 1, 0, 0, 0},
                 {0, 0, 0, 1, 0, 0, 0, 0},
@@ -73,7 +101,8 @@ public class 숲속의기사 {
                 {4, 0, 0, 0, 1, 0, 0, 0},
                 {4, 1, 0, 0, 1, 0, 0, 0},
                 {4, 0, 0, 0, 0, 0, 1, 2}}));
-        System.out.println(solution(new int[][]{{4, 1, 0, 1, 0},
+        System.out.println(T.solution(new int[][]{
+                {4, 1, 0, 1, 0},
                 {0, 1, 0, 1, 0},
                 {0, 0, 2, 3, 4},
                 {0, 1, 0, 1, 0}}));
