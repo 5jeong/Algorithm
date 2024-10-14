9b763db18b4935d90050497555d91092f6c1d96b
5jeong <wjdgns4385@naver.com>
2024-10-14 11:30:17 +0900

    2024-10-14 study

---
+++ baekjoon/gold/Baekjoon_1987.java
@@ -0,0 +1,50 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1987 {
+    static int r, c;
+    static char[][] board;
+    static boolean[] alp; // 알파벳 체크
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int ans;
+
+    static void dfs(int x, int y, int L) {
+        if (alp[board[x][y] - 65]) {
+            ans = Math.max(ans, L);
+        } else {
+            alp[board[x][y] - 65] = true;
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
+                    dfs(nx, ny, L + 1);
+                }
+            }
+            alp[board[x][y] - 65] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        board = new char[r][c];
+        ans = 1;
+        alp = new boolean[26];
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+        dfs(0, 0, 0);
+
+        System.out.println(ans);
+    }
+}
