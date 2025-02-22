e740274598e38830aa660852486fea1341102ddc
5jeong <wjdgns4385@naver.com>
2025-02-22 17:44:23 +0900

    2025-02-22 study

--- baekjoon/gold/Baekjoon_5427.java
+++ baekjoon/gold/Baekjoon_5427.java
@@ -3,19 +3,20 @@ package baekjoon.gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon_5427 {
-    static int ans, w, h;
+    static int w, h;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     static char[][] board;
-    static int[][] dis;
+    static int[][] fireTime; // 불이 도달하는 최소시간
     static int[][] visited;
-    static Queue<Point> queue;
     static StringBuilder sb = new StringBuilder();
 
     static class Point {
@@ -37,50 +38,46 @@ public class Baekjoon_5427 {
             h = Integer.parseInt(st.nextToken());
 
             board = new char[h][w];
-            dis = new int[h][w];
+            fireTime = new int[h][w];
             visited = new int[h][w];
             for (int i = 0; i < h; i++) {
-                Arrays.fill(dis[i], Integer.MAX_VALUE);
+                Arrays.fill(fireTime[i], Integer.MAX_VALUE);
             }
+
+            List<Point> fires = new ArrayList<>();
+            Point start = null;
+
             for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                String s = st.nextToken();
+
+                String s = br.readLine();
                 for (int j = 0; j < w; j++) {
                     board[i][j] = s.charAt(j);
+                    if (board[i][j] == '*') {
+                        fires.add(new Point(i, j));
+                    }
+                    if (board[i][j] == '@') {
+                        start = new Point(i, j);
+                    }
                 }
             }
 
             // 불 bfs
-            queue = new LinkedList<>();
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (board[i][j] == '*') {
-                        queue.offer(new Point(i, j));
-                    }
-                }
-            }
-            fire();
+            fire(fires);
 
             // 상근 bfs
-            queue = new LinkedList<>();
-            visited = new int[h][w];
-
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (board[i][j] == '@') {
-                        queue.offer(new Point(i, j));
-                    }
-                }
-            }
-            escape();
+            escape(start);
 
         }
         System.out.println(sb);
     }
 
-    static void fire() {
+    static void fire(List<Point> fires) {
+        Queue<Point> queue = new LinkedList<>();
+        for (Point fire : fires) {
+            queue.offer(fire);
+            fireTime[fire.x][fire.y] = 0;
+        }
 
-        int L = 0;
         while (!queue.isEmpty()) {
             int len = queue.size();
             for (int i = 0; i < len; i++) {
@@ -88,39 +85,44 @@ public class Baekjoon_5427 {
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = temp.x + dx[dir];
                     int ny = temp.y + dy[dir];
-                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && board[nx][ny] != '#' && visited[nx][ny] == 0) {
-                        visited[nx][ny] = 1;
-                        dis[nx][ny] = Math.min(dis[nx][ny], L + 1);
+                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && board[nx][ny] != '#'
+                            && fireTime[nx][ny] == Integer.MAX_VALUE) {
+                        fireTime[nx][ny] = fireTime[temp.x][temp.y] + 1;
                         queue.offer(new Point(nx, ny));
                     }
                 }
             }
-            L++;
         }
     }
 
-    static void escape() {
+    static void escape(Point start) {
 
-        int L = 0;
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start.x][start.y] = 1;
+
+        int time = 0;
         while (!queue.isEmpty()) {
             int len = queue.size();
+            time++;
             for (int i = 0; i < len; i++) {
                 Point temp = queue.poll();
 
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = temp.x + dx[dir];
                     int ny = temp.y + dy[dir];
+                    // 탈출 성공
                     if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
-                        sb.append(L+1).append("\n");
+                        sb.append(time).append("\n");
                         return;
                     }
-                    if (board[nx][ny] == '.' && dis[nx][ny] > L+1 && visited[nx][ny] == 0) {
+                    // 이동 가능 여부 확인
+                    if (board[nx][ny] == '.' && fireTime[nx][ny] > time  && visited[nx][ny] == 0) {
                         visited[nx][ny] = 1;
                         queue.offer(new Point(nx, ny));
                     }
                 }
             }
-            L++;
         }
         sb.append("IMPOSSIBLE").append("\n");
     }
