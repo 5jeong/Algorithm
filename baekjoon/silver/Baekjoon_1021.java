cc0cc5a90dcc0c3f4a62c230ef342bc0029ce025
5jeong <wjdgns4385@naver.com>
2025-04-06 19:05:11 +0900

    2025-04-04 study

--- baekjoon/silver/Baekjoon_1021.java
+++ baekjoon/silver/Baekjoon_1021.java
@@ -1,9 +1,9 @@
 package baekjoon.silver;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 회전하는 큐
@@ -13,49 +13,27 @@ public class Baekjoon_1021 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
 
-        Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
-            deque.add(i);
+            queue.add(i);
         }
-        List<Integer> pick = new ArrayList<>();
+        List<Integer> picks = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            pick.add(sc.nextInt());
+            picks.add(sc.nextInt());
         }
+
         int ans = 0;
-        Deque<Integer> leftDeque = new ArrayDeque<>(deque);
-        Deque<Integer> rightDeque = new ArrayDeque<>(deque);
-        for (int x : pick) {
-            int leftCnt = 0;
-            int rightCnt = 0;
-            while (leftDeque.peekFirst() != x) {
-                left(leftDeque);
-                leftCnt++;
+        for (int x : picks) {
+            int cnt = 0;
+            while (x != queue.peek()) {
+                queue.add(queue.poll());
+                cnt++;
             }
-            leftDeque.pollFirst();
-            while (rightDeque.peekFirst() != x) {
-                right(rightDeque);
-                rightCnt++;
-            }
-            rightDeque.pollFirst();
-            if(leftCnt < rightCnt){
-                rightDeque = new ArrayDeque<>(leftDeque);
-                ans += leftCnt;
-            }else{
-                leftDeque = new ArrayDeque<>(rightDeque);
-                ans += rightCnt;
-            }
+            ans += Math.min(Math.abs(queue.size() - cnt), cnt);
+            queue.poll();
         }
         System.out.println(ans);
     }
 
-    private static void left(Deque<Integer> deque) {
-        int temp = deque.pollFirst();
-        deque.addLast(temp);
-    }
-
-    private static void right(Deque<Integer> deque) {
-        int temp = deque.pollLast();
-        deque.addFirst(temp);
-    }
 }
